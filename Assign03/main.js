078e4d237f6999cda59e69db46824cbf44dc96bb
Talha Ashfaq <talhaashfaq45@yahoo.com>
2024-03-09 16:02:34 +0500

    first commit

---
+++ Assign03/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment # 3
+// Storing a famous quote and its author
+let famousquote = "If everything around seems dark, look again, you may be the light.";
+let author = "Rumi";
+// print a quote with its author name
+console.log(`${author} said, "${famousquote}"`);
---
+++ Assign03/main.ts
@@ -0,0 +1,10 @@
+// Assignment # 3
+// Storing a famous quote and its author
+
+let famousquote: string= "If everything around seems dark, look again, you may be the light."
+
+let author : string= "Rumi"
+
+// print a quote with its author name
+
+console.log(`${author} said, "${famousquote}"`);
